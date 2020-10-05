0b2a2da8186c229d3ee22c130b80f87622ae15de
chinoportela <matiasportela94@gmail.com>
2020-10-04 21:00:32 -0300

    Added Cine Model with setters and getters

--- MoviePass/Models/Cine/Cine.php
+++ MoviePass/Models/Cine/Cine.php
@@ -2,18 +2,53 @@
 
     namespace Cine;
 
-    use Ubicacion as Ubicacion;
+    use Ubicacion\Direccion as Direccion;
+    use Ubicacion\Localidad as Localidad;
 
     class Cine 
     {
         private $id;
         private $nombre;
-        private $ubicacion;
+        private $direccion;
+        private $localidad;
 
         public function __construct($nombre = "")
         {
             $this->nombre=$nombre;
-            $this->ubicacion = new Ubicacion();
+            $this->direccion = new Direccion();
+            $this->localidad = new Localidad();
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
+
+        public function getDireccion(){
+            return $this->direccion;
+        }
+
+        public function getLocalidad(){
+            return $this->localidad;
+        }
+
+        public function setId($id){
+            $this->id = $id;
+        }
+
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function setDireccion(Direccion $direccion){
+            $this->direccion = $direccion;
+        }
+
+        public function setLocalidad(Localidad $localidad){
+            $this->localidad = $localidad;
         }
 
     }
