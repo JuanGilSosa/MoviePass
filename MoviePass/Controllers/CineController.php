0095db53ec5c1081023b9bdcebe6e3d7c2b10ca8
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-10 17:52:39 -0300

    solo acomode indentacion

--- MoviePass/Controllers/CineController.php
+++ MoviePass/Controllers/CineController.php
@@ -4,42 +4,48 @@
     use DAO\CineDAO as CineDAO;
     use DAO\LocalidadDAO as LocalidadDAO;
     use DAO\DireccionDAO as DireccionDAO;
+    use DAO\PaisDAO as PaisDAO;
     use Models\Cine\Cine as Cine;
     use Models\Ubicacion\Direccion as Direccion;
     use Models\Ubicacion\Localidad as Localidad;
-
+    use Models\Ubicacion\Pais as Pais;
     class CineController
     {
         private $cineDAO;
         private $localidadDAO;
         private $direccionDAO;
+        private $paisDAO;
 
-        public function __construct()
-        {
+        public function __construct(){
             $this->cineDAO = new CineDAO();
             $this->localidadDAO = new LocalidadDAO();
             $this->direccionDAO = new DireccionDAO();
+            $this->paisDAO = new PaisDAO();
         }
 
-        public function ShowAddView()
-        {
+        public function ShowAddView(){
             require_once(VIEWS_PATH."addCine.php");
         }
 
-        public function ShowListView()
-        {
+        public function ShowListView(){
             $cines = $this->cineDAO->GetAll();
             $direcciones = $this->direccionDAO->GetAll();
             $localidades = $this->localidadDAO->GetAll();
+            #$paises = $this->paisDAO->GetAll();
 
             require_once(VIEWS_PATH."cinesList.php");
         }
 
-        public function Add($nombre, $email, $numeroDeContacto, $calle, $numero, $piso, $departamento, $localidad, $codigoPostal, $provincia, $pais)
+        public function Add(
+            $nombre, $email, $numeroDeContacto, $calle, 
+            $numero, $piso, $departamento, $localidad, 
+            $codigoPostal, $provincia, $idPais
+        )
         {
             $cine = new Cine($nombre, $email, $numeroDeContacto);
             $direccion = new Direccion($calle, $numero, $piso, $departamento);
-            $localidad = new Localidad($localidad, $codigoPostal, $provincia, $pais);
+            #$this->paisDAO->getForId($idPais)->getId() obtengo la instancia del pais para usar el metodo getId() y setearlo en localidad
+            $localidad = new Localidad($localidad, $codigoPostal, $provincia, $idPais);
 
             /* 
                 $cine->setDireccionId($direccionId);
@@ -54,6 +60,7 @@
 
             $this->ShowAddView();
         }
+
 
     }
 ?>
