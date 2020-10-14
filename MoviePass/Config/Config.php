f95b15f7ef7a8bf174e9316dee826f0258458264
chinoportela <matiasportela94@gmail.com>
2020-10-13 22:09:22 -0300

    Agregadas API_KEY y API_URL en el global scope

--- MoviePass/Config/Config.php
+++ MoviePass/Config/Config.php
@@ -7,6 +7,12 @@ define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("FONTAWESOME_PATH", FRONT_ROOT.VIEWS_PATH . "fontawesome/css/all.css");
+
+//
+define("API_URL", "https://api.themoviedb.org/3/movie/");
+define("API_KEY", "a67565019e2b3ed72b43911ab7692772");
+
+
 ?>
 
 
