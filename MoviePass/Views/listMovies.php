45599d0a0bd7f687a566cf49a86df66ad481075d
chinoportela <matiasportela94@gmail.com>
2020-10-15 17:34:03 -0300

    removed FILTRAR button in listMovies

--- MoviePass/Views/listMovies.php
+++ MoviePass/Views/listMovies.php
@@ -22,7 +22,6 @@
                                              <option value="<?php echo $g['id'] ?>" required><?php echo $g['name']; ?></option>
                                    <?php }?>
                               </select>
-                              <input type="submit" value="Filtrar">
                          </div>
                     </form>
                </div>
