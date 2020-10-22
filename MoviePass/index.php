319feb1657c97ce80617bb815bddc64c98e6728c
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-22 17:45:34 -0300

    agregado de funcion ob_start() para funcionar con los header - no lo veo fiable

--- MoviePass/index.php
+++ MoviePass/index.php
@@ -1,5 +1,7 @@
 <?php
  
+ 	ob_start();
+
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
@@ -10,7 +12,7 @@
 	use Config\Autoload as Autoload;
 	use Config\Router 	as Router;
 	use Config\Request 	as Request;
-		
+	
 	Autoload::start();
 
 	session_start();
