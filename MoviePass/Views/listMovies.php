cd2365e9b44f962f9d8eec87723610a11d1effa0
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-22 15:01:32 -0300

    modificado para ver las portadas de las peliculas

--- MoviePass/Views/listMovies.php
+++ MoviePass/Views/listMovies.php
@@ -26,58 +26,27 @@
                       	</div>
                     </form>
                </div>
-          	<section id="listado" class="mb-5">
-          
-               <table id="dt-vertical-scroll" class="table  table-striped bg-dark text-white" cellspacing="0" width="100%" style="margin:0px; padding:0px;">
-               <thead>
-                    <tr>
-                         <th class="th-sm">Titulo
-                         </th>
-                         <th class="th-sm">Descripcion
-                         </th>
-                         <th class="th-sm">Generos
-                         </th>
-                         <th class="th-sm">Lanzamiento
-                         </th>
-                         <th class="th-sm">Rating
-                         </th>
-                    </tr>
-               </thead>
-                    <tbody>
-                    <?php
-                              foreach ($peliculas as $pelicula){
-                                ?>
-                                    <tr>
-                                        <td><?php echo $pelicula->getTitle() ?></td>
-                                        <td><?php echo $pelicula->getOverview() ?></td>
-                                        <td><?php $this->generosDAO->ShowGenres($this->generosDAO->getGenresNamesById($pelicula->getGenres()));?></td>
-                                        <td><?php echo $pelicula->getReleaseDate() ?></td>
-                                        <td><?php echo $pelicula->getVoteAverage() ?></td>
-                                    </tr>
-                                <?php
-                            }
-                         ?>
-                    </tbody>
-          	</table>
-          	</section>
-          	<!--
+
+			
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-12">
-						<div class="flex-viewport" style="overflow: hidden; position: relative;"><ul class="slides" style="width: 2400%; transition-duration: 0s; transform: translate3d(0px, 0px, 0px);">
-							<li class="item slider-item" style="width: 146px; margin-right: 10px; float: left; display: block;">
-								<figure class="thumb" style="height: 215px;">
-									<a href="https://cartelera.elperiodico.com/pelicula/vitalina-varela-35353.html"><img src="https://image.tmdb.org/t/p/original/yPeG1RQm5Am0eslu0IwUEJ4VXND.jpg" style="width:100%; height: 215px; object-fit: cover;" alt="Vitalina Varela" draggable="true"></a>
-								</figure>
-								
-								<div class="txt">
-									<h6 class="title"><a href="https://cartelera.elperiodico.com/pelicula/vitalina-varela-35353.html" title="Vitalina Varela">Vitalina Varela</a></h6>
-								</div>
-							</li>
+					<form action="<?php echo FRONT_ROOT.'Pelicula/ShowMovieDescription'?>" method="POST">
+						<div class="col-xs-3 col-sm-12 col-md-12" >
+							<?php
+								foreach ($peliculas as $pelicula){
+							?>
+							<div class="text-center">
+							<button name="idMovie" type="submit" value="<?php echo $pelicula->getId()?>">
+								<img style="width: 146px; margin-right: 10px; float: left; display: inline;" src="https://image.tmdb.org/t/p/original<?php echo $pelicula->getPosterPath()?>" alt="Imagen">
+							</button>
+								<h6 class="title"><?php echo $pelicula->getTitle()?></h6>
+							</div>
+							<?php
+							}?>
 						</div>
 					</div>
 				</div>
-			</div>-->
+			</form>
     </section>
 </main>
 
