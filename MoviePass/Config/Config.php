a5bb954e0a830da2323daaea8a7b147ba2b2c6ee
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-22 14:57:15 -0300

    agregado de key 2

--- MoviePass/Config/Config.php
+++ MoviePass/Config/Config.php
@@ -8,11 +8,10 @@ define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
 define("FONTAWESOME_PATH", FRONT_ROOT.VIEWS_PATH . "fontawesome/css/all.css");
 define("MDB_PATH", FRONT_ROOT.VIEWS_PATH . "BootstrapMaterialDesign/css/mdb.min.css");
-
 //
 define("API_URL", "https://api.themoviedb.org/3/movie/");
-define("API_KEY", "a67565019e2b3ed72b43911ab7692772");
-
+define("API_KEY1", "a67565019e2b3ed72b43911ab7692772");
+define("API_KEY2", "48621040dbb9c7f28355bff08c002197");
 
 ?>
 
