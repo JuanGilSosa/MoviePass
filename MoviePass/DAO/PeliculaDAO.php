0bed91ebb4c5b2e32a73b5679a86d5a57ca59476
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-22 22:19:15 -0300

    borrado de comentarios innecesario

--- MoviePass/DAO/PeliculaDAO.php
+++ MoviePass/DAO/PeliculaDAO.php
@@ -38,8 +38,8 @@
                 --example of json--see footer of this file
             */    
             /*    
-                Asi descargo cosas. Por ejemplo, si es $var = file_get_contents("https://google.com"); descarga la estructura de google y la muestro
-                con un echo $var;
+                Asi descargo cosas. Por ejemplo, si es $var = file_get_contents("https://google.com"); descarga la estructura de 
+                google y la muestro con un echo $var;
                 En este caso si se lee la documentacion("example of how to get a json::")en la seccion Try It out(al lado de Definition),
                 y ponemos nuestra key en api_key, presionamos SEND REQUEST muestra un json, siendo que el LINK que aparece al lado del button que envia 
                 el request es el que utilizaremos para obtener la lista de peliculas
@@ -47,7 +47,7 @@
                 Todo este proceso es similar para obtener los generos
             */
             $peliculas = file_get_contents(
-                'https://api.themoviedb.org/3/movie/now_playing?api_key=48621040dbb9c7f28355bff08c002197&language=es-ES&page=1'
+                API_URL.'now_playing?api_key='.API_KEY1.'&language=es-ES&page=1'
             );
 			
             $decode = json_decode($peliculas, true);
@@ -136,7 +136,7 @@
 
     public function getTrailerKey($idPelicula){
       $trailerJson = file_get_contents(
-        "https://api.themoviedb.org/3/movie/{$idPelicula}/videos?api_key=48621040dbb9c7f28355bff08c002197&language=es-ES"
+        API_URL."{$idPelicula}/videos?api_key=".API_KEY1."&language=es-ES"
       );
       $trailerArray = ($trailerJson) ? json_decode($trailerJson, true) : array();
       return $trailerArray['results'][0]['key'];
@@ -196,143 +196,4 @@
     },
     "total_pages": 67
 }
-
-/**********example of MOVIE DESCRIPTION json************* */
-
-
-{
-  "adult": false,
-  "backdrop_path": "/kriOlxu4KVEjd0ZaY3dW7YYyP4z.jpg",
-  "belongs_to_collection": null,
-  "budget": 0,
-  "genres": [
-    {
-      "id": 35,
-      "name": "Comedia"
-    },
-    {
-      "id": 18,
-      "name": "Drama"
-    },
-    {
-      "id": 10770,
-      "name": "Película de TV"
-    }
-  ],
-  "homepage": "https://www.hbo.com/movies/bad-education",
-  "id": 530723,
-  "imdb_id": "tt8206668",
-  "original_language": "en",
-  "original_title": "Bad Education",
-  "overview": "Frank Tassone, uno de los superintendentes más destacados del distrito escolar de Roslyn en Nueva York se desvive a diario para que la educación de sus alumnos sea la mejor. Pero de forma paralela, este se lucra con dinero público para llevar una vida llena de lujos. Así, Tassone y su personal, amigos y familiares, se convierten en los principales sospechosos del mayor escándalo de malversación de fondos ocurrido en una escuela pública de toda la historia de los Estados Unidos.",
-  "popularity": 232.89,
-  "poster_path": "/57H8KjN5qHYJjaq5SaUPsuVypPh.jpg",
-  "production_companies": [
-    {
-      "id": 99963,
-      "logo_path": "/wi5JP4jpVBeb9qK97nhICkMurNZ.png",
-      "name": "Sight Unseen Pictures",
-      "origin_country": "US"
-    },
-    {
-      "id": 7625,
-      "logo_path": "/kzq4ibnNeZrp00zpBWv3st1QCOh.png",
-      "name": "Automatik Entertainment",
-      "origin_country": "US"
-    },
-    {
-      "id": 7429,
-      "logo_path": "/6in9uMqxXEHx5XgYgkeRBpZ4rPw.png",
-      "name": "HBO Films",
-      "origin_country": "US"
-    },
-    {
-      "id": 132844,
-      "logo_path": null,
-      "name": "Slater Hall Pictures",
-      "origin_country": ""
-    }
-  ],
-  "production_countries": [
-    {
-      "iso_3166_1": "US",
-      "name": "United States of America"
-    }
-  ],
-  "release_date": "2020-09-23",
-  "revenue": 0,
-  "runtime": 108,
-  "spoken_languages": [
-    {
-      "iso_639_1": "en",
-      "name": "English"
-    }
-  ],
-  "status": "Released",
-  "tagline": "Algunas personas aprenden de manera difícil.",
-  "title": "La estafa (Bad Education)",
-  "video": false,
-  "vote_average": 6.9,
-  "vote_count": 38
-}
-
--->
-
-<!--
-        /!**** ESTAS FUNCIONES HACEN LO MISMO QUE EL CODIGO DEL DAO *****!/
-
-
-        function GetMoviesPlaying(){
-            
-            $get_data = $this->callAPI('GET', 'https://api.themoviedb.org/3/movie/now_playing?api_key=a67565019e2b3ed72b43911ab7692772&language=es-AR', false);
-            $response = json_decode($get_data, true);
-            //$errors = $response['response']['errors'];
-            $data = $response['results'];
-
-            return $data;
-            
-        }
-
-        function GetMovieById($idMovie){
-
-            $get_data = $this->callAPI('GET', 'https://api.themoviedb.org/3/movie/' . $idMovie .'?api_key=a67565019e2b3ed72b43911ab7692772&language=es-AR', false);
-            $response = json_decode($get_data, true);
-            //$errors = $response['response']['errors'];
-            $data = $response;
-
-            return $data;
-        }
-
-        function callAPI($method, $url, $data){
-            $curl = curl_init();
-            switch ($method){
-               case "POST":
-                  curl_setopt($curl, CURLOPT_POST, 1);
-                  if ($data)
-                     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                  break;
-               case "PUT":
-                  curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-                  if ($data)
-                     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
-                  break;
-               default:
-                  if ($data)
-                     $url = sprintf("%s?%s", $url, http_build_query($data));
-            }
-            // OPTIONS:
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-               'APIKEY: a67565019e2b3ed72b43911ab7692772',
-               'Content-Type: application/json',
-            ));
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-            // EXECUTE:
-            $result = curl_exec($curl);
-            if(!$result){die("Connection Failure");}
-            curl_close($curl);
-            return $result;
-         }
-
 -->
