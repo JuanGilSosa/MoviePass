431acbf2594824f0783532410dcb6efedf3df935
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-23 19:07:18 -0300

    mas bien seria login admin

---
+++ MoviePass/Views/register-adm.php
@@ -0,0 +1,11 @@
+<main class="height-100">
+    <div class="container"> 
+        <div class="card">
+            <div class="card-body px-lg-5 pt-0">
+                <form action="<?php echo FRONT_ROOT.'Admin/LoginAdmin'?>"  method="POST" class="md-form" style="color: #757575;">
+                    <input name="pw" type="password" id="materialLoginFormPassword" class="form-control" placeholder="Contraseña">
+                    <button type="submit" class="btn btn-outline-info waves-effect btn-block" >Ingresar</button>
+            </div>
+        </div>
+    </div>
+</main>
