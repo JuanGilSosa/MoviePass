ea05e7ba1fe676de59ad0a24d4380c59365caf96
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-10-26 14:42:01 -0300

    define para interactuar con la bdd

--- MoviePass/Config/Config.php
+++ MoviePass/Config/Config.php
@@ -13,9 +13,9 @@ define("API_URL", "https://api.themoviedb.org/3/movie/");
 define("API_KEY1", "a67565019e2b3ed72b43911ab7692772");
 define("API_KEY2", "48621040dbb9c7f28355bff08c002197");
 
-define("DB_HOST" , "");
-define("DB_NAME" , "");
-define("DB_USER" , "");
+define("DB_HOST" , "localhost");
+define("DB_NAME" , "moviepass");
+define("DB_USER" , "root");
 define("DB_PASS" , "");
 
 define("PW_ADMIN" , "holamundo");
