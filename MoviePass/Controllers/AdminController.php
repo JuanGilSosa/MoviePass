2ceb304f7e1bc9233efe1c308264784910126120
chinoportela <matiasportela94@gmail.com>
2020-11-05 18:53:21 -0300

    corrected SessionHelper methods in AdminController

--- MoviePass/Controllers/AdminController.php
+++ MoviePass/Controllers/AdminController.php
@@ -1,30 +1,26 @@
 <?php namespace Controllers;
 
     use Models\Users\Admin as Admin;
-    use DAO\AdminDAO as AdminDAO;
 
-    use DAO\CineDAO as CineDAO;
-    use Model\Cine\Cine as Cine;
-    use DAO\PaisDAO as PaisDAO;
-    use Models\Ubicacion\Pais as Pais;
-    use DAO\ProvinciaDAO as ProvinciaDAO;
-    use Models\Ubicacion\Provincia as Provincia;
-    use DAO\CiudadDAO as CiudadDAO;
-    use Models\Ubicacion\Ciudad as Ciudad;
+    use DAO\MovieDAO as MovieDAO;
+    use Model\Theatre\Theatre as Theatre;
+    use DAO\CountryDAO as CountryDAO;
+    use Models\Location\Country as Country;
+    use DAO\ProvinceDAO as ProvinceDAO;
+    use Models\Location\Province as Province;
+    use DAO\CityDAO as CityDAO;
+    use Models\Location\City as City;
 
     use Helpers\SessionHelper as SessionHelper;
 
     class AdminController{
 
         private $adminDAO;
-
-        public function __construct(){
-            $this->cineController = new CineController();
-        }
+        
 
         public function LoginAdmin($pw){
             if($pw == PW_ADMIN){
-                SessionHelper::setOnSession('adminLogged',true);
+                SessionHelper::SetSession('adminLogged',true);
                 #$_SESSION['adminLogged'] = true;
                 HomeController::Index();
             }else{
@@ -34,12 +30,12 @@
         }
         
         public function ShowAddView(){
-            if(SessionHelper::HayUsuario('adminLogged')){
-               /* $paisDAO = new PaisDAO(); 
+            if(SessionHelper::isSession('adminLogged')){
+               /* $paisDAO = new CountryDAO(); 
                 $paises = $paisDAO->GetAll();
-                $provinciaDAO = new ProvinciaDAO();
+                $provinciaDAO = new ProvinceDAO();
                 $provincias = $provinciaDAO->GetAll();
-                $ciudadDAO = new CiudadDAO();
+                $ciudadDAO = new CityDAO();
                 $ciudades = $ciudadDAO->GetAll();
                 */
                 ViewsController::ShowAddCineView();
