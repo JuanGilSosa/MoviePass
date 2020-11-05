7450b2a6f8611fb76c7389c2e678a3a686c0b67b
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-11-05 13:43:40 -0300

    Billboard dinamico - sin persistir

--- MoviePass/Models/Pelicula/Billboard.php
+++ MoviePass/Models/Pelicula/Billboard.php
@@ -2,25 +2,18 @@
     
     class Billboard{
 
-        private $id;
         private $functions;
         private $date;
-        private $active;
 
-        public function __construct($id ="", $functions ="", $date){
-            $this->id = $id;
+        public function __construct($functions ="", $date){
             $this->funciones = array();
             $this->date = $date;
-            $this->active = 1;
         }
-
-        public function setId($id){$this->id = $id;}
+        
         public function setFunctions($functions){$this->functions = $functions;}
         public function setDate($date){$this->date = $date;}
-        public function getId(){return $this->id;}
         public function getFunctions(){return $this->funciones;}
         public function getDate(){return $this->date;}
-        public function isActive(){return $this->active;}
 
     }
 ?>
