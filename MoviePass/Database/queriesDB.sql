0d7db60f9d6062295124de0a4f94268f6cded446
chinoportela <matiasportela94@gmail.com>
2020-11-05 17:10:13 -0300

    translated database queries to english

--- MoviePass/Database/queriesDB.sql
+++ MoviePass/Database/queriesDB.sql
@@ -2,79 +2,94 @@ USE moviepass;
 
 #CREACION DE TABLAS
 
-CREATE TABLE Paises(
-    idPais INT NOT NULL AUTO_INCREMENT,
-    namePais VARCHAR(30),
-    CONSTRAINT pk_idPais PRIMARY KEY(idPais)
+CREATE TABLE Countries(
+    countryId INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    CONSTRAINT pk_countryId PRIMARY KEY(countryId)
 );
 
-CREATE TABLE Provincias(
-    idProvincia INT NOT NULL AUTO_INCREMENT,
-    nameProvincia VARCHAR(30) NOT NULL,
-    idPais INT,
-    CONSTRAINT pk_idProvincia PRIMARY KEY(idProvincia),
-    CONSTRAINT fk_idPais FOREIGN KEY(idPais) REFERENCES Paises(idPais)
+CREATE TABLE Provinces(
+    provinceId INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    countryId INT,
+    CONSTRAINT pk_provinceId PRIMARY KEY(provinceId),
+    CONSTRAINT fk_countryId FOREIGN KEY(countryId) REFERENCES Countries(countryId) ON DELETE CASCADE
 );
 
-CREATE TABLE Ciudades(
-    codigoPostal INT NOT NULL,
-    nameCiudad VARCHAR(30) NOT NULL,
-    idProvincia INT,
-    CONSTRAINT pk_codigoPostal PRIMARY KEY(codigoPostal),
-    CONSTRAINT fk_idProvincia FOREIGN KEY(idProvincia) REFERENCES Provincias(idProvincia)
+CREATE TABLE Cities(
+    zipCodes INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    provinceId INT,
+    CONSTRAINT pk_zipCode PRIMARY KEY(zipCode),
+    CONSTRAINT fk_provinceId FOREIGN KEY(provinceId) REFERENCES Provinces(provinceId) ON DELETE CASCADE
 );
 
-CREATE TABLE Direcciones(
-    idDireccion INT NOT NULL AUTO_INCREMENT,
-    calle VARCHAR(30) NOT NULL,
-    numero INT NOT NULL,
-    piso INT,
-    codigoPostal INT,
-    CONSTRAINT pk_idDireccion PRIMARY KEY(idDireccion),
-    CONSTRAINT fk_codigoPostal FOREIGN KEY(codigoPostal) REFERENCES Ciudades(codigoPostal)
+CREATE TABLE Adresses(
+    adressId INT NOT NULL AUTO_INCREMENT,
+    street VARCHAR(30) NOT NULL,
+    number INT NOT NULL,
+    floor INT,
+    zipCode INT,
+    CONSTRAINT pk_adressId PRIMARY KEY(adressId),
+    CONSTRAINT fk_zipCode FOREIGN KEY(zipCode) REFERENCES Cities(zipCode) ON DELETE CASCADE
 );
 
-CREATE TABLE Cines(
-    idCine INT NOT NULL AUTO_INCREMENT, 
-    nombre VARCHAR(30), 
+CREATE TABLE Theatres(
+    theatreID INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30), 
     email VARCHAR(30), 
-    numeroDeContacto VARCHAR(15), 
-    idDireccion INT,
+    phoneNumber VARCHAR(15), 
+    adressId INT,
     active BOOLEAN, 
-    CONSTRAINT pk_idCine PRIMARY KEY(idCine),
-    CONSTRAINT fk_idDirecionn FOREIGN KEY(idDireccion) REFERENCES direcciones(idDireccion)
+    CONSTRAINT pk_theatreId PRIMARY KEY(theatreId),
+    CONSTRAINT fk_adressId FOREIGN KEY(adressId) REFERENCES adresses(adressId) ON DELETE CASCADE
 );
 
-CREATE TABLE Salas(
-    idSala INT NOT NULL AUTO_INCREMENT, 
-    nombre VARCHAR(30), 
-    precio INT, 
-    capacidad INT,
-    tipo VARCHAR(5),
-    CONSTRAINT pk_idSala PRIMARY KEY(idSala)
+CREATE TABLE Cinemas(
+    cinemaId INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30), 
+    price INT, 
+    capacity INT,
+    type VARCHAR(5),
+    CONSTRAINT pk_cinemaId PRIMARY KEY(cinemaId) ON DELETE CASCADE
 );
 
-CREATE TABLE Funciones(
-    idFuncion INT NOT NULL AUTO_INCREMENT,
-    horaInicio VARCHAR(30) NOT NULL,
-    horaFin VARCHAR(30) NOT NULL,
-    idPelicula INT,
+CREATE TABLE Billboard(
+    billboardId INT NOT NULL AUTO_INCREMENT,
+    startDate date NOT NULL,
+    endDate date NOT NULL,
+    active BOOLEAN NOT NULL,
+    CONSTRAINT pk_billboardId PRIMARY KEY(billboardId)
+);
+
+CREATE TABLE carteleraXfuncion(
+    billboardId INT,
+    showtimeId INT,
+    CONSTRAINT fk_billboardId FOREIGN KEY(billboardId) REFERENCES Billboard(billboardId) ON DELETE CASCADE,
+    CONSTRAINT fk_showtimeId FOREIGN KEY(showtimeId) REFERENCES Showtimes(showtimeId) ON DELETE CASCADE
+);
+
+CREATE TABLE Showtimes(
+    showtimeId INT NOT NULL AUTO_INCREMENT,
+    startTime VARCHAR(30) NOT NULL,
+    endTime VARCHAR(30) NOT NULL,
+    movieId INT,
     active BOOLEAN,
-    CONSTRAINT pk_idFuncion PRIMARY KEY(idFuncion)
+    CONSTRAINT pk_showtimeId PRIMARY KEY(showtimeId) ON DELETE CASCADE
 );
 
-CREATE TABLE salaXfunciones(
-    idSala INT,
-    idFuncion INT,
-    CONSTRAINT fk_idSala FOREIGN KEY(idSala) REFERENCES Salas(idSala),
-    CONSTRAINT fk_idFuncion FOREIGN KEY(idFuncion) REFERENCES Funciones(idFuncion)
+CREATE TABLE showtimesXcinemas(
+    showtimeId INT,
+    cinemaId INT,
+    CONSTRAINT fk_showtimeId FOREIGN KEY(showtimeId) REFERENCES Cinemas(showtimeId),
+    CONSTRAINT fk_showtimeId FOREIGN KEY(showtimeId) REFERENCES Showtimes(showtimeId)
 );
 
-CREATE TABLE salaXcine(
-    idSala INT,
-    idCine INT,
-    CONSTRAINT pk_idSala FOREIGN KEY(idSala) REFERENCES salas(idSala),
-    CONSTRAINT pk_idCine FOREIGN KEY(idCine) REFERENCES cines(idCine)
+CREATE TABLE cinemasXtheatres(
+    cinemaId INT,
+    theatreId INT,
+    CONSTRAINT pk_cinemaId FOREIGN KEY(cinemaId) REFERENCES cinemas(cinemaId),
+    CONSTRAINT pk_theatreId FOREIGN KEY(theatreId) REFERENCES thatres(theatreId)
 );
 
 CREATE TABLE members(
@@ -84,31 +99,19 @@ CREATE TABLE members(
     password varchar(30) NOT NULL,
     firstName varchar(30) NOT NULL,
     lastName varchar(30) NOT NULL,
-    numeroTarjetaDeCredito INT,
+    creditCardNumber INT,
     CONSTRAINT pk_idMember PRIMARY KEY(idMember)
 );
 
-CREATE TABLE Cartelera(
-    idCartelera INT NOT NULL AUTO_INCREMENT,
-    fecha VARCHAR(30) NOT NULL,
-    active BOOLEAN NOT NULL,
-    CONSTRAINT pk_idCartelera PRIMARY KEY(idCartelera)
-);
 
-CREATE TABLE carteleraXfuncion(
-    idCartelera INT,
-    idFuncion INT,
-    CONSTRAINT fk__idCartelera FOREIGN KEY(idCartelera) REFERENCES Cartelera(idCartelera),
-    CONSTRAINT fk__idFuncion FOREIGN KEY(idFuncion) REFERENCES Funciones(idFuncion)
-);
 
 #########OTRAS QUERIES#########
 
 #AGREGANDO PAIES
-INSERT INTO Paises(namePais) VALUES('Argentina'),('Brasil'),('Chile'),('Uruguay');
+INSERT INTO COUNTRIES(NAME) VALUES('Argentina'),('Brasil'),('Chile'),('Uruguay');
 
 #AGREGANDO PROVINCIAS
-INSERT INTO Provincias(nameProvincia, idPais) VALUES('Buenos Aires',1),
+INSERT INTO Provinces(name, countryId) VALUES('Buenos Aires',1),
                                                             ('Santa Fe',1),
                                                             ('Cordoba',1),
                                                             ('Paysandu',4),
@@ -119,7 +122,7 @@ INSERT INTO Provincias(nameProvincia, idPais) VALUES('Buenos Aires',1),
                                                             ('Rio Grande del Sur',2);
 
 #AGREGANDO CIUDADES
-INSERT INTO Ciudades(codigoPostal, nameCiudad, idProvincia) VALUES(7600,'Mar del Plata',1),
+INSERT INTO Cities(zipCode, name, provinceId) VALUES(7600,'Mar del Plata',1),
                                                                             (1016,'Ciudad Autonoma de Buenos Aires',1),
                                                                             (4555,'Santa Fe',2),
                                                                             (5468,'Caros Paz',3);
