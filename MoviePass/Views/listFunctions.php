00e0bd7c30879607b2a1190ae0675194fc4aef74
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-11-06 21:53:27 -0300

    listas de las funciones cargadas -- falta filtrarlas

--- MoviePass/Views/listFunctions.php
+++ MoviePass/Views/listFunctions.php
@@ -1,33 +1,37 @@
-<?php 
-require_once('nav.php');
-foreach($billboards as $billboard):
-    foreach($billboard->getFunctions() as $function):
-        if(!empty($function) && !empty($function->getMovie())):
-?>
-<div class="border box-large col-lg-3 col-md-4 col-sm-6 col-xs-12 no-p">
-    <article class="data-sheet new">
-        <a href="/espectaculos/cine/la-parte-oscura_9733.html">
-            <figure>
-                <div class="premiere"><p><?php $nameSala = $function->getSala()->getNombre(); echo $nameSala;?></p></div>
-                <img class="lazyload" alt="La parte oscura" data-src="/espectaculos/cine/image/la-parte-oscura_9733.jpg" src="/espectaculos/cine/image/la-parte-oscura_9733.jpg" pinger-seen="true">
-            </figure>
-        </a>
-        <div class="mt form-group">
-            <a href="/espectaculos/cine/la-parte-oscura_9733.html">
-            <h4><?php 
-                    $title = $function->getMovie()->getTitle();
-                    echo $title; 
-                 ?>
-            </h4>
-            </a>
-            <p>Genero: <span>OBTENER DE PHP</span></p>
-            <p class="clase">Votos: <?php echo $function->getMovie()->getVoteAverage();?></p>
-            <p> Inicia: <?php echo $function->getHoraInicio(); ?> </p>
-        </div>
-    </article>
-</div>
-<?php 
-        endif;
-    endforeach;
-endforeach; 
-?>
+<?php require_once('nav.php'); ?>
+<div class="row">
+    <?php 
+    foreach($billboards as $billboard):
+        foreach($billboard->getFunctions() as $function):
+            if(!empty($function) && !empty($function->getMovie())):
+    ?>
+                <div class="col-lg-2 col-md-6">
+                    <div class="card card-cascade narrower">
+                        <div class="view view-cascade overlay">
+                            <img class="" src="<?php $link = $function->getMovie()->getPosterPath(); echo REQUEST_IMG.'/w300/'.$link; ?>" alt="Portada">
+                        </div>
+                        <a>
+                            <div class="mask rgba-white-slight waves-effect waves-light"></div>
+                        </a>
+                        
+                    </div>
+                    <div class="card-body card-body-cascade">
+                        <h5 class="red-text"><i class="fas fa-film"></i> <?php $nameSala = $function->getSala()->getNombre(); echo $nameSala;?></h5>
+                        <h4 class="card-title text-dark"><?php 
+                                                    $title = $function->getMovie()->getTitle();
+                                                    echo $title; 
+                                                    ?>
+                        </h4>
+                        <p class="card-text">Comienzo: <?php echo $function->getHoraInicio();?></p>
+                        <p class="card-text">Generos: <?php echo '{los generos}';?></p>
+                    
+                        <a value="<?php echo ''; ?>" href="<?php echo FRONT_ROOT . 'Pelicula/ShowMovieDescription?idPelicula=' ?>" type="button" class="btn btn-unique">Reservar</a>
+                        <a href="<?php echo FRONT_ROOT . 'Pelicula/ShowMovieDescription?idPelicula='.$function->getMovie()->getId(); ?>" type="button" class="btn btn-unique">Ver Info</a>        
+                    </div>
+                </div>
+    <?php 
+            endif;
+        endforeach;
+    endforeach; 
+    ?>
+    </div> 
