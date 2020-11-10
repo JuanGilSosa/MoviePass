471e321be883084078432c3fbd613e14b52aed4a
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-11-10 13:50:09 -0300

    carga el ticket en el session pero no puede agarrar dos tickets

--- MoviePass/Controllers/CartController.php
+++ MoviePass/Controllers/CartController.php
@@ -6,8 +6,13 @@
     use Database\MoviesDAO as MoviesDAO;
     use Models\Shopping\Ticket as Ticket;
     use Models\Shopping\Cart as Cart;
-    class CartController{
 
+    use Controllers\ShowtimeController as ShowtimeController;
+
+    use Helpers\SessionHelper;
+
+    class CartController{   
+        
         private $ticketDAO;
         private $showTimeDAO;
         private $cinemaDAO;
@@ -21,7 +26,7 @@
             $this->movieDAO = new MoviesDAO();
             $this->cart = new Cart();
         }
-
+/*
         public function AddShowtime($idShowTime){
             $showTime = $this->showTimeDAO->GetShowtimeById($idShowTime);
             if(!empty($showTime) && is_object($showTime)){ #trato asi la condicion porque solo voy a traer una funcion, nada mas(object)
@@ -37,6 +42,28 @@
 
         public function AddTicket($arrOfTickets){
             $this->ticketDAO->Add($arrOfTickets);
+        }*/
+
+        public function AddShowTime($idShowTime){
+            $showTime = $this->showTimeDAO->GetShowtimeById($idShowTime);
+            if(!empty($showTime) && is_object($showTime)){ #trato asi la condicion porque solo voy a traer una funcion, nada mas(object)
+                $cinema = $this->cinemaDAO->GetCinema_showtimesXcinema($idShowTime);
+                $movie = $this->movieDAO->getMovieById($showTime->GetMovie());
+                $showTime->SetMovie($movie);
+                $showTime->SetCinema($cinema);
+                $myTicket =  new Ticket(0,$showTime); echo 'VER LINEA 32 CartController '.$idShowTime;
+                $this->cart->PushTicket($myTicket);
+
+                if(!SessionHelper::isSession('CART')){
+                    SessionHelper::SetOnIndex('CART',0,$myTicket);#$_SESSION['CART'][0] = $myTicket;
+                }else{
+                    $length = SessionHelper::LengthOfKey('CART');
+                    SessionHelper:SetOnIndex('CART',$length,$myTicket);#$_SESSION['CART'][$length] = $myTicket;
+                }
+                $showTimeController = new ShowtimeController();
+                $showTimeController->ShowShowtimes();
+                #ViewsController::ShowCartView();
+            }
         }
     }
 
