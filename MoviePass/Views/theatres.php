a5b2faeca632cda459bf56190fea06c7b3e4d35f
Juan Gil Sosa <juanmanuels499@gmail.com>
2020-11-17 17:04:44 -0300

    cambio de los alert a info y success - agregado de precio cuando muestra las funciones

--- MoviePass/Views/theatres.php
+++ MoviePass/Views/theatres.php
@@ -12,7 +12,7 @@ require_once('nav.php');
                          #echo "<small>" . $message . "</small>";
                     ?>
                          <div class="container">
-                              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                              <div class="alert alert-info alert-dismissible fade show" role="alert">
                                    <?php echo $message ?>
                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
